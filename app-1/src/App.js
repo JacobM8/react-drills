cb1ee26a16c2d0149f1213789f9fee1619ba0a9c
Jake Morrison <iamjacobmorrison@gmail.com>
2020-07-11 12:08:38 -0600

    app-1 completed

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+// Create a basic react app where you type in a text box and it shows up as text on the DOM.
+// See https://github.com/DevMountain/react-drills
+// "npx create-react-app folder-name" command in terminal to create new react app
+
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    userInput: ''
+  }
+
+  inputHandler = input => {
+    this.setState({userInput: input})
+  }
+
+  render(){
+    return(
+      <div style={{textAlign: 'center'}}>
+        <input onChange={e=>this.inputHandler(e.target.value)}/>
+        <h1>{this.state.userInput}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
